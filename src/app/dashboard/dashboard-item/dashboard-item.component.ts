038330b4a19d1273beed8c16774536f8a3b8dca2
tuongtran1106 <tuongtran1106@gmail.com>
2024-07-09 10:14:50 +0700

    Scoping CSS Styles to Components & Configuring View Encapsulation, Interact via @HostListener & @HostBinding

--- src/app/dashboard/dashboard-item/dashboard-item.component.ts
+++ src/app/dashboard/dashboard-item/dashboard-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, Input } from '@angular/core';
+import { Component, input, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard-item',
@@ -6,6 +6,10 @@ import { Component, input, Input } from '@angular/core';
   imports: [],
   templateUrl: './dashboard-item.component.html',
   styleUrl: './dashboard-item.component.css',
+  // encapsulation: ViewEncapsulation.None,
+  // host: {
+  //   class: 'dashboard-item'
+  // }
 })
 export class DashboardItemComponent {
   // @Input({ required: true }) image!: { src: string; alt: string };
