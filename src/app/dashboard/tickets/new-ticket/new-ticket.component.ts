392cde5f7bd75cc21ed13ac79cd5dc5962cae3a6
tuongtran1106 <tuongtran1106@gmail.com>
2024-07-09 15:54:56 +0700

    Handle submit form output and loop ticket item

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef, OnInit, viewChild, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, EventEmitter, OnInit, output, Output, viewChild, ViewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms'
 
 import { ButtonComponent } from '../../../shared/button/button.component';
@@ -14,6 +14,8 @@ import { ControlComponent } from "../../../shared/control/control.component";
 export class NewTicketComponent implements OnInit, AfterViewInit {
   // @ViewChild('form') form?: ElementRef<HTMLFormElement>;
   private form = viewChild.required<ElementRef<HTMLFormElement>>('form');
+  // @Output() add = new EventEmitter<{title: string; text: string}>();
+  add = output<{title: string; text: string}>();
 
   ngOnInit(): void {
     console.log('ON INIT');
@@ -28,8 +30,7 @@ export class NewTicketComponent implements OnInit, AfterViewInit {
   }
 
   onSubmit(title: string, ticketText: string) {
-    console.log(title);
-    console.log(ticketText);
+    this.add.emit({title: title, text: ticketText});
     this.form().nativeElement.reset();
   }
 }
