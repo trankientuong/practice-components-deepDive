ab14cf8fbc1c424c2cef03da208518785c2d55f1
tuongtran1106 <tuongtran1106@gmail.com>
2024-07-09 16:37:14 +0700

    Apply two-way binding and output function

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -15,6 +15,8 @@ export class NewTicketComponent implements OnInit, AfterViewInit {
   // @ViewChild('form') form?: ElementRef<HTMLFormElement>;
   private form = viewChild.required<ElementRef<HTMLFormElement>>('form');
   // @Output() add = new EventEmitter<{title: string; text: string}>();
+  enteredTitle = '';
+  enteredText = '';
   add = output<{title: string; text: string}>();
 
   ngOnInit(): void {
@@ -29,8 +31,10 @@ export class NewTicketComponent implements OnInit, AfterViewInit {
     console.log(this.form().nativeElement);
   }
 
-  onSubmit(title: string, ticketText: string) {
-    this.add.emit({title: title, text: ticketText});
-    this.form().nativeElement.reset();
+  onSubmit() {
+    this.add.emit({ title: this.enteredTitle, text: this.enteredText });
+    // this.form().nativeElement.reset();
+    this.enteredTitle = '';
+    this.enteredText = '';
   }
 }
